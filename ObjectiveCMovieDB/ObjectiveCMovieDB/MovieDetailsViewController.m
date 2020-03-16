d9227fb3e2bb00366aac19b4366803e566025995
Felipe Girardi <felipegirardifg@gmail.com>
2020-03-16 18:39:20 -0300

    Implement initial movieDetails layout

---
+++ ObjectiveCMovieDB/ObjectiveCMovieDB/MovieDetailsViewController.m
@@ -0,0 +1,25 @@
+//
+//  MovieDetailsViewController.m
+//  ObjectiveCMovieDB
+//
+//  Created by Felipe Girardi on 16/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+#import "MovieDetailsViewController.h"
+
+@interface MovieDetailsViewController ()
+
+@end
+
+@implementation MovieDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
+
+// [textView setTextContainerInset:UIEdgeInsetsZero];
