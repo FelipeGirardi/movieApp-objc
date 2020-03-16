3658f45c3202d019c2905a5e50ac2beebdc32bfc
henriqueconte <henriqueconte6@gmail.com>
2020-03-16 16:12:54 -0300

    Separated files in the determined architecture

---
+++ ObjectiveCMovieDB/ObjectiveCMovieDB/Headers/MainScreenView.h
@@ -0,0 +1,13 @@
+//
+//  MainScreenView.h
+//  ObjectiveCMovieDB
+//
+//  Created by Henrique Figueiredo Conte on 16/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+#ifndef MainScreenView_h
+#define MainScreenView_h
+
+
+#endif /* MainScreenView_h */
---
+++ ObjectiveCMovieDB/ObjectiveCMovieDB/View/MainScreenView.m
@@ -0,0 +1,9 @@
+//
+//  MainScreenView.m
+//  ObjectiveCMovieDB
+//
+//  Created by Henrique Figueiredo Conte on 16/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
