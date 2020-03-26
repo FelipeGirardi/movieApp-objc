fdad1c4ab981b83033b02970c01d549afae893ec
henriqueconte <henriqueconte6@gmail.com>
2020-03-26 19:10:50 -0300

    Started studying tests on objc

---
+++ ObjectiveCMovieDB/ObjectiveCMovieDB/Tests/NetworkTests.h
@@ -0,0 +1,21 @@
+//
+//  NetworkTests.h
+//  ObjectiveCMovieDB
+//
+//  Created by Henrique Figueiredo Conte on 26/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+#ifndef NetworkTests_h
+#define NetworkTests_h
+//#import <XCTest/XCTest.h>
+
+@interface NetworkTests : NSObject
+    
+- (void) setup;
+
+
+@end
+
+
+#endif /* NetworkTests_h */
