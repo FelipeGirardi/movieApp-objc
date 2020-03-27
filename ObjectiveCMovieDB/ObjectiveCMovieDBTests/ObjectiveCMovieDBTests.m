aff5fc51901065427444be04728c70a302176c11
henriqueconte <henriqueconte6@gmail.com>
2020-03-27 13:00:40 -0300

    Implemented basic api test

---
+++ ObjectiveCMovieDB/ObjectiveCMovieDBTests/ObjectiveCMovieDBTests.m
@@ -0,0 +1,54 @@
+//
+//  ObjectiveCMovieDBTests.m
+//  ObjectiveCMovieDBTests
+//
+//  Created by Henrique Figueiredo Conte on 27/03/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "MainScreenNetwork.h"
+
+@interface ObjectiveCMovieDBTests : XCTestCase
+
+@property MainScreenNetwork *mainNetwork;
+
+@end
+
+@implementation ObjectiveCMovieDBTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    [super setUp];
+    
+    _mainNetwork = [[MainScreenNetwork alloc] init];
+    
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+- (void) testApiActivity {
+    
+    [_mainNetwork getDataFrom:@"https://api.themoviedb.org/3/movie/upcoming?api_key=77d63fcdb563d7f208a22cca549b5f3e&language=en-US&page=1" completion: ^(NSMutableArray * moviesList) {
+       
+        XCTAssertFalse([moviesList count] == 0);
+        
+    }];
+}
+
+@end
